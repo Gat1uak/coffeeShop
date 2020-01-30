d807108544f046c5ea8bc4795113b98942f56910
Gatluak Tholbok <tholbok@Gatluaks-MacBook-Pro.local>
2020-01-31 00:29:31 +1100

    add navToggle/hide and show

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
 // hide preloader
 // all the images scripts links have finished loading
 
-// window event list
-window.addEventListener('load', function () {
-   setTimeout(function () {
+(function () {
+   // EventListeners function
+   eventListeners();
+   function eventListeners() {
+      const ui = new UI();
+      // window event list
+      window.addEventListener('load', function () {
+         setTimeout(function () {
+            ui.hidePreloader();
+         }, 3000)
+      })
+      // Navigation Toggle Button
+      document.querySelector('.navBtn').addEventListener('click', function () {
+         ui.showNav();
+      })
+   }
+   // Constructor function object
+   function UI() { }
+   UI.prototype.hidePreloader = function () {
       document.querySelector('.preloader').style.display = 'none';
-   },3000)
-})
+   }
+   UI.prototype.showNav = function () {
+      document.querySelector('.nav').classList.toggle('nav__show');
+   }
 
+})();
