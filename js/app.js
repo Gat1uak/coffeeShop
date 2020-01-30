48f55106cb9ddfb96c87881ad3a1122b74dc5b45
Gatluak Tholbok <tholbok@Gatluaks-MacBook-Pro.local>
2020-01-30 23:28:34 +1100

    add appjs and hide preloader when window loads

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// hide preloader
+// all the images scripts links have finished loading
+
+// window event list
+window.addEventListener('load', function () {
+   setTimeout(function () {
+      document.querySelector('.preloader').style.display = 'none';
+   },3000)
+})
+
